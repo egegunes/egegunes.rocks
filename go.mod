7202d59ab018163885e994b5c1f7cb0cfe40326c
Ege Güneş <egegunes@gmail.com>
2019-06-17 22:14:36 +0300

    Add dynamodb

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/egegunes/egegunes.rocks
 
 go 1.12
 
-require github.com/gin-gonic/gin v1.4.0
+require (
+	github.com/aws/aws-sdk-go v1.20.2
+	github.com/gin-gonic/gin v1.4.0
+	github.com/guregu/dynamo v1.2.1 // indirect
+)
--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,12 +15,66 @@ type comment struct {
 	CreatedAt time.Time
 }
 
+func createTable(db *dynamodb.DynamoDB, tableName string) error {
+	input := &dynamodb.CreateTableInput{
+		BillingMode: aws.String("PAY_PER_REQUEST"),
+		AttributeDefinitions: []*dynamodb.AttributeDefinition{
+			{
+				AttributeName: aws.String("Message"),
+				AttributeType: aws.String("S"),
+			},
+			{
+				AttributeName: aws.String("Time"),
+				AttributeType: aws.String("N"),
+			},
+		},
+		KeySchema: []*dynamodb.KeySchemaElement{
+			{
+				AttributeName: aws.String("Message"),
+				KeyType:       aws.String("HASH"),
+			},
+			{
+				AttributeName: aws.String("Time"),
+				KeyType:       aws.String("RANGE"),
+			},
+		},
+		TableName: aws.String("Comments"),
+	}
+	_, err := db.CreateTable(input)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func tableExists(db *dynamodb.DynamoDB, tableName string) bool {
+	input := &dynamodb.DescribeTableInput{
+		TableName: aws.String(tableName),
+	}
+
+	_, err := db.DescribeTable(input)
+
+	return err != nil
+}
+
 func main() {
+	db := dynamodb.New(session.New(), &aws.Config{
+		Region:   aws.String("eu-central-1"),
+		Endpoint: aws.String("http://localhost:8000"),
+	})
+
+	tableName := "Comments"
+
+	if !tableExists(db, tableName) {
+		createTable(db, tableName)
+	}
+
 	router := gin.Default()
 	router.LoadHTMLFiles("templates/index.tmpl")
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"title": "Main website",
 			"comments": []comment{
 				comment{"deneme", time.Now()},
 				comment{"deneme2", time.Now()},
