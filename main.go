08c79c72d296bee59c6ab15d33186b680f1c6eef
Ege Güneş <egegunes@gmail.com>
2019-06-21 08:27:07 +0300

    Add dynamodb get and put methods

--- main.go
+++ main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/gin-gonic/gin"
 )
 
 type comment struct {
+	Name      string
 	Message   string
-	CreatedAt time.Time
+	CreatedAt string
 }
 
 func createTable(db *dynamodb.DynamoDB, tableName string) error {
@@ -20,25 +23,25 @@ func createTable(db *dynamodb.DynamoDB, tableName string) error {
 		BillingMode: aws.String("PAY_PER_REQUEST"),
 		AttributeDefinitions: []*dynamodb.AttributeDefinition{
 			{
-				AttributeName: aws.String("Message"),
+				AttributeName: aws.String("Name"),
 				AttributeType: aws.String("S"),
 			},
 			{
-				AttributeName: aws.String("Time"),
-				AttributeType: aws.String("N"),
+				AttributeName: aws.String("CreatedAt"),
+				AttributeType: aws.String("S"),
 			},
 		},
 		KeySchema: []*dynamodb.KeySchemaElement{
 			{
-				AttributeName: aws.String("Message"),
+				AttributeName: aws.String("Name"),
 				KeyType:       aws.String("HASH"),
 			},
 			{
-				AttributeName: aws.String("Time"),
+				AttributeName: aws.String("CreatedAt"),
 				KeyType:       aws.String("RANGE"),
 			},
 		},
-		TableName: aws.String("Comments"),
+		TableName: aws.String(tableName),
 	}
 	_, err := db.CreateTable(input)
 
@@ -49,14 +52,48 @@ func createTable(db *dynamodb.DynamoDB, tableName string) error {
 	return nil
 }
 
-func tableExists(db *dynamodb.DynamoDB, tableName string) bool {
+func tableExists(db *dynamodb.DynamoDB, tableName string) error {
 	input := &dynamodb.DescribeTableInput{
 		TableName: aws.String(tableName),
 	}
 
 	_, err := db.DescribeTable(input)
 
-	return err != nil
+	return err
+}
+
+func getComments(db *dynamodb.DynamoDB, tableName string) ([]comment, error) {
+	var comments []comment
+
+	input := &dynamodb.ScanInput{
+		ConsistentRead: aws.Bool(true),
+		TableName:      aws.String(tableName),
+	}
+	result, err := db.Scan(input)
+
+	if err != nil {
+		return comments, err
+	}
+
+	dynamodbattribute.UnmarshalListOfMaps(result.Items, &comments)
+
+	return comments, err
+}
+
+func putComment(db *dynamodb.DynamoDB, tableName string, c comment) error {
+	av, err := dynamodbattribute.MarshalMap(c)
+	if err != nil {
+		return err
+	}
+
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String(tableName),
+	}
+
+	_, err = db.PutItem(input)
+
+	return err
 }
 
 func main() {
@@ -67,19 +104,35 @@ func main() {
 
 	tableName := "Comments"
 
-	if !tableExists(db, tableName) {
-		createTable(db, tableName)
+	if err := tableExists(db, tableName); err != nil {
+		log.Println("table does not exist, creating new")
+		err := createTable(db, tableName)
+		if err != nil {
+			log.Printf("can't create table: %s", err)
+		}
 	}
 
 	router := gin.Default()
 	router.LoadHTMLFiles("templates/index.tmpl")
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"comments": []comment{
-				comment{"deneme", time.Now()},
-				comment{"deneme2", time.Now()},
-			},
-		})
+		comments, err := getComments(db, tableName)
+		if err != nil {
+			log.Printf("can't get comments: %s", err)
+		}
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{"comments": comments})
+	})
+	router.POST("/", func(c *gin.Context) {
+		name := c.PostForm("name")
+		message := c.PostForm("message")
+		err := putComment(db, tableName, comment{name, message, time.Now().Format(time.RFC3339)})
+		if err != nil {
+			log.Printf("can't put comment: %s", err)
+		}
+		comments, err := getComments(db, tableName)
+		if err != nil {
+			log.Printf("can't get comments: %s", err)
+		}
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{"comments": comments})
 	})
 	router.Run(":8080")
 }
